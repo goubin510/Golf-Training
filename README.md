3a0d4954a1685eadb8670b04cda745838b625ebb
Goubin <agoubin@deloitte.fr>
2019-05-06 11:06:07 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ExpressAPIBased
+"# Golf-Training" 
--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var express = require('express'),
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/express', { useNewUrlParser: true }); 
+// mongoose.connect('mongodb://username:password@host:port/database?options...');
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
